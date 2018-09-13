fc32abf131db8dc7b393b9e33f63cdeb09c9c560
Dylan Hand <ingrathis@gmail.com>
2018-09-13 09:32:20 -0400

    New utility: MTG Decklist Creator

--- Simple_Tests/test.py
+++ Simple_Tests/test.py
@@ -1,7 +1,4 @@
-x = 2 
+x = 2
 y = 2 
 z = x + y 
-print z 
-
-print y 
-print x 
+print (z) 
---
+++ Utilities/decklist_creator.py
@@ -0,0 +1,44 @@
+print(" ")
+print(" ")
+print("##################################################")
+print("#                                                #")
+print("#   Magic: The Gathering Decklist Creation Tool  #")
+print("#                                                #")
+print("#               v0.0.1                           #")
+print("##################################################")
+print(" ")
+print(" ")
+print(" ")
+
+def main():
+    menu = {}
+    menu['1']="Create Decklist" 
+    menu['2']="Load Decklist"
+    menu['3']="Exit"
+    mBool = True 
+    while mBool == True: 
+        options=menu.keys()
+        for entry in options: 
+            print (entry, menu[entry])
+ 
+        selection=input("Please select the number of the option you would like: ") 
+        if selection =='1': 
+            deckcreate() 
+            mBool = False
+        elif selection == '2': 
+            print ("delete") 
+        elif selection == '3':
+            mBool = False
+            print(" ")
+            print("Exiting program.")
+            break  
+        else: 
+            print ("Unknown Option Selected!")
+         
+
+def deckcreate():
+        print("Deck creation (placeholder)")
+        selection=input("Please select the number of the dank option you would like: ") 
+        print (selection)
+main()
+#deckcreate()
